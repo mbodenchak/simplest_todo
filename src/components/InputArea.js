55b8ca2bf2549274b5f76a0ba1b9768509de4119
mbodenchak <mbodenchak@outlook.com>
2022-06-01 11:56:50 -0700

    basic styling

---
+++ src/components/InputArea.js
@@ -0,0 +1,12 @@
+const InputArea = () => {
+  return (
+    <div className="form">
+      <input type="text" />
+      <button>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+};
+
+export default InputArea;
